ff886b880e9cff9b7503ca28f03af48cee8fb763
Dodo <wmmyo987@gmail.com>
2025-08-15 08:55:57 +0700

    เช็คและแก้ไขระบบเว็บไซค์

--- src/components/admin/components/MenuContent.js
+++ src/components/admin/components/MenuContent.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
@@ -12,51 +11,34 @@ import Diversity3Icon from '@mui/icons-material/Diversity3';
 import CategoryIcon from '@mui/icons-material/Category';
 import ExtensionIcon from '@mui/icons-material/Extension';
 
-
 export default function MenuContent() {
   return (
     <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
       <List dense>
-          <ListItem key="1"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton selected href="/admin">
-              <ListItemIcon><DashboardIcon/></ListItemIcon>
-              <ListItemText primary='แดชบอร์ด' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem key="2"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton href="/admin/order">
-              <ListItemIcon><ShoppingCartIcon/></ListItemIcon>
-              <ListItemText primary='รายการสั่งซื้อ' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem key="3"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton href="/admin/customer">
-              <ListItemIcon><GroupsIcon/></ListItemIcon>
-              <ListItemText primary='ลูกค้า' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem key="4"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton href="#">
-              <ListItemIcon><Diversity3Icon/></ListItemIcon>
-              <ListItemText primary='พนักงาน' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem key="5"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton href="#">
-              <ListItemIcon><CategoryIcon/></ListItemIcon>
-              <ListItemText primary='ประเภทสินค้า' />
-            </ListItemButton>
-          </ListItem>
-          
-          <ListItem key="6"  disablePadding sx={{ display: 'block' }}>
-            <ListItemButton href="#">
-              <ListItemIcon><ExtensionIcon/></ListItemIcon>
-              <ListItemText primary='สินค้า' />
-            </ListItemButton>
-          </ListItem>
+        <ListItemButton selected href="/admin">
+          <ListItemIcon><DashboardIcon /></ListItemIcon>
+          <ListItemText primary='แดชบอร์ด' />
+        </ListItemButton>
+        <ListItemButton href="/admin/order">
+          <ListItemIcon><ShoppingCartIcon /></ListItemIcon>
+          <ListItemText primary='รายการสั่งซื้อ' />
+        </ListItemButton>
+        <ListItemButton href="/admin/customer">
+          <ListItemIcon><GroupsIcon /></ListItemIcon>
+          <ListItemText primary='ลูกค้า' />
+        </ListItemButton>
+        <ListItemButton href="#">
+          <ListItemIcon><Diversity3Icon /></ListItemIcon>
+          <ListItemText primary='พนักงาน' />
+        </ListItemButton>
+        <ListItemButton href="#">
+          <ListItemIcon><CategoryIcon /></ListItemIcon>
+          <ListItemText primary='ประเภทสินค้า' />
+        </ListItemButton>
+        <ListItemButton href="#">
+          <ListItemIcon><ExtensionIcon /></ListItemIcon>
+          <ListItemText primary='สินค้า' />
+        </ListItemButton>
       </List>
     </Stack>
   );
